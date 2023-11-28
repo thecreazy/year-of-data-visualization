4c77accaea920ad113b89f1ffa318314b8f454c5
thecreazy <canella_riccardo@icloud.com>
2023-11-28 11:14:26 +0100

    feat(195): Romanian temperatures

---
+++ src/app/day/195/layout.jsx
@@ -0,0 +1,8 @@
+export const metadata = {
+  title: `Day 195 | Romanian's Historical maximum temperatures`,
+  description: `Historical Romanian data from 1979 to 2022 on maximum temperatures calculated from 3 different locations`,
+};
+
+export default function PageLayout({ children }) {
+  return children;
+}
