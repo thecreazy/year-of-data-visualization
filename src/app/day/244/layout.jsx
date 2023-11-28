6769638b140a36609db2bec95c35ebc085a7ddf0
thecreazy <canella_riccardo@icloud.com>
2023-11-28 11:29:27 +0100

    feat(244): Swedish temperatures

---
+++ src/app/day/244/layout.jsx
@@ -0,0 +1,8 @@
+export const metadata = {
+  title: `Day 244 | Swedish's Historical maximum temperatures`,
+  description: `Historical Swedish data from 1979 to 2022 on maximum temperatures calculated from 3 different locations`,
+};
+
+export default function PageLayout({ children }) {
+  return children;
+}
