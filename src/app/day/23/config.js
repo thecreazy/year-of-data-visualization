18822bc8cd8781f7ac86dffa3acf342d0a2c4fa1
thecreazy <canella_riccardo@icloud.com>
2023-12-21 16:49:45 +0100

    feat(23): Women’s share of government ministerial positions

---
+++ src/app/day/23/config.js
@@ -0,0 +1,4 @@
+export const infos = {
+  title: `Women’s share of government ministerial positions`,
+  description: `Proportion of women and girls in goverment ministerial position in 2020`,
+};
---
+++ src/app/day/23/layout.jsx
@@ -0,0 +1,8 @@
+export const metadata = {
+  title: `Day 13 | Women’s share of government ministerial positions`,
+  description: `Proportion of women and girls in goverment ministerial position in 2020`,
+};
+
+export default function PageLayout({ children }) {
+  return children;
+}
