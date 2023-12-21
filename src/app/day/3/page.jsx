0f21ca831ea9f599a342f5e600670154ebd8fddc
thecreazy <canella_riccardo@icloud.com>
2023-12-21 13:55:07 +0100

    fix(3): fixed line chart

--- src/app/day/3/page.jsx
+++ src/app/day/3/page.jsx
@@ -20,7 +20,6 @@ const Page3 = () => {
         <h3 className='py-2 font-mono text-3xl max-md:text-2xl text-center flex max-md:flex-col justify-center items-center'>
           Last 5 year stock price
         </h3>
-        {JSON.stringify(lineData)}
         <div className='mb-[20px] flex h-[550px] w-full justify-center max-md:h-[300px] max-xl:h-[500px]'>
           <NivoLine
             data={lineData}
