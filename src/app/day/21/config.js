9276d88d924452c01869ae4edf867156b8827d8f
thecreazy <canella_riccardo@icloud.com>
2023-12-21 14:47:06 +0100

    feat(21): Francesco Totti Statistics

---
+++ src/app/day/21/config.js
@@ -0,0 +1,27 @@
+export const infos = {
+  title: `Francesco Totti Statistics`,
+  description: `Francesco Totti, born on September 27, 1976, in Rome, Italy, is a retired Italian footballer widely regarded as one of the greatest players in AS Roma's history. Totti spent his entire professional career with AS Roma, spanning from 1992 to 2017.`,
+  awards: [
+    {
+      title: 'AS Roma (1992-2017):',
+      data: [
+        'Serie A Title: 1 (2000-2001 season).',
+        'Coppa Italia: 2 (2006-2007, 2007-2008).',
+        'Supercoppa Italiana: 2 (2001, 2007).',
+      ],
+    },
+    {
+      title: 'Italy National Team:',
+      data: ['FIFA World Cup: 1 (2006).'],
+    },
+    {
+      title: 'Individual Honors:',
+      data: [
+        'Serie A Italian Footballer of the Year: Multiple times.',
+        'FIFA World Cup Best Young Player: 1998.',
+        'Included in UEFA Team of the Year.',
+        'Named in the FIFA 100 list.',
+      ],
+    },
+  ],
+};
---
+++ src/app/day/21/layout.jsx
@@ -0,0 +1,8 @@
+export const metadata = {
+  title: 'Day 21 | Francesco Totti',
+  description: `Francesco Totti, born on September 27, 1976, in Rome, Italy, is a retired Italian footballer widely regarded as one of the greatest players in AS Roma's history. Totti spent his entire professional career with AS Roma, spanning from 1992 to 2017.`,
+};
+
+export default function PageLayout({ children }) {
+  return children;
+}
