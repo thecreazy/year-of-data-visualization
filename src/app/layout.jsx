3d9f3f305d16c814c92f5be45a819afae03908bb
thecreazy <canella_riccardo@icloud.com>
2024-01-16 23:04:19 +0100

    feat(analitycs): added new speedinsight vercel package

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,4 +1,5 @@
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import { Inter } from 'next/font/google';
 import Script from 'next/script';
 
@@ -108,6 +109,7 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
