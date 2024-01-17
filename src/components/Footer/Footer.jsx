d4731922179c776422ee4c46ee5aeb615ab9e5b2
thecreazy <canella_riccardo@icloud.com>
2024-01-17 09:28:57 +0100

    feat(footer): link on footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   const divs = 23;
   return (
     <footer>
-      <div style={{ marginTop: '-23px', backgroundColor: '#191919' }}>
+      <div className='f-container' style={{ marginTop: '-23px' }}>
         {new Array(divs).fill(0).map((_, index) => {
           return (
             <div
@@ -15,6 +15,31 @@ const Footer = () => {
           );
         })}
       </div>
+      <section className='infos text-center pt-4 pb-4'>
+        <p>
+          <a href='https://yodv.canellariccardo.it' rel='noopener noreferrer'>
+            YODV{' '}
+          </a>
+          a project of{' '}
+          <a
+            href='https://canellariccardo.it'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            Riccardo Canella
+          </a>
+        </p>
+        <p>
+          Check the opensource code on{' '}
+          <a
+            href='https://github.com/thecreazy/year-of-data-visualization'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            Github
+          </a>
+        </p>
+      </section>
     </footer>
   );
 };
