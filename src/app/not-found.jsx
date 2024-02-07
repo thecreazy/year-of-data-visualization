6aad6ffeff6a4eccf6e3a41213cd1c3063b39f2f
thecreazy <canella_riccardo@icloud.com>
2024-02-07 19:21:09 +0100

    feat(404): not found page

---
+++ src/app/not-found.jsx
@@ -0,0 +1,52 @@
+import './notfound.css';
+
+export default function NotFound() {
+  return (
+    <>
+      <a
+        href='https://codepen.io/ZonFire99/full/njdls/'
+        class='viewFull'
+        target='_parent'
+      >
+        View in full it looks way better :)
+      </a>
+
+      <div class='error'>
+        <div class='wrap'>
+          <div class='404'>
+            <pre>
+              <code>
+                <span>
+                  <span class='green'>&lt;!</span>
+                  <span>DOCTYPE html</span>
+                  <span class='green'>&gt;</span>
+                </span>
+                <span class='orange'>&lt;html&gt;</span>
+                <span class='orange'>&nbsp;&lt;style&gt;</span>
+                {`  * {`}
+                <span>
+                  <span class='green'>&nbsp;&nbsp;&nbsp;everything</span>:
+                  <span class='blue'>awesome</span>;
+                </span>
+                {`  }`}
+                <span class='orange'>&nbsp;&lt;/style&gt;</span>
+                <span class='orange'>&nbsp;&lt;body&gt;</span>
+                <br />
+                &nbsp;&nbsp;ERROR 404! FILE NOT FOUND!
+                <br />
+                <span class='comment'>
+                  &nbsp;&nbsp;&lt;!--The page you are looking for,
+                  <br /> &nbsp;&nbsp;is not where you think it is.--&gt;
+                </span>
+                <span class='orange'></span>
+                <span class='info' />
+                <span class='orange'>&nbsp;&lt;/body&gt;</span>
+                <span class='orange'>&lt;/html&gt;</span>
+              </code>
+            </pre>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
