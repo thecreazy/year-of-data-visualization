d0eebdbacf69ebc04a16444aa4585c1aea76aed5
thecreazy <canella_riccardo@icloud.com>
2024-02-23 12:47:55 +0100

    feat(video): created videos per youtube shorts

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,16 @@
 const { defineConfig } = require('cypress');
 
+const viewport =
+  process.env.VIDEO_TYPE === 'youtube'
+    ? {
+        viewportWidth: 1080,
+        viewportHeight: 1920,
+      }
+    : {
+        viewportWidth: 1366,
+        viewportHeight: 800,
+      };
+
 module.exports = defineConfig({
   component: {
     devServer: {
@@ -7,12 +18,21 @@ module.exports = defineConfig({
       bundler: 'webpack',
     },
   },
-  viewportWidth: 1366,
-  viewportHeight: 800,
   video: true,
   videosFolder: './public/videos',
   e2e: {
     setupNodeEvents(on, config) {},
     baseUrl: 'http://localhost:3000',
+    setupNodeEvents(on, config) {
+      on('before:browser:launch', (browser = {}, launchOptions) => {
+        if (browser.family === 'chromium' && browser.name !== 'electron') {
+          launchOptions.args.push(
+            `--window-size=${viewport.viewportWidth},${viewport.viewportHeight}`
+          );
+          return launchOptions;
+        }
+      });
+    },
   },
+  ...viewport,
 });
