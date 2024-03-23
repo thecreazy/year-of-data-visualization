1c79ff1be4982667c24d6694f73546f55f2704fd
thecreazy <canella_riccardo@icloud.com>
2024-03-23 09:28:41 +0100

    chore(core): added .nvmrc

---
+++ src/app/day/89/config.js
@@ -0,0 +1,4 @@
+export const infos = {
+  title: `Minimum age for legal marriage without parental consent`,
+  description: `Analysis for each country on the difference in legal age for marriage`,
+};
