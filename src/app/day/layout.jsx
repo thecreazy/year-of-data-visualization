a29693d7700f7f0083bb5572d893ab086e53f66f
thecreazy <canella_riccardo@icloud.com>
2024-03-31 23:06:43 +0200

    chore(core): use next 14.1.4

--- src/app/day/layout.jsx
+++ src/app/day/layout.jsx
@@ -1,23 +1,23 @@
+'use client';
+
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 import { Marvel } from '@internal/components/Marvel/Marvel';
 
 import './layout.css';
 
 export default function DayLayout(props) {
+  const pathname = usePathname();
   const { children } = props;
+  const day = pathname.split('/')[2];
   return (
-    <main
-      id={`data-${children.props.childPropSegment}`}
-      className='relative min-h-screen px-[32px]'
-    >
-      {children.props.childPropSegment === '51' && <Marvel />}
+    <main id={`data-${day}`} className='relative min-h-screen px-[32px]'>
+      {day === '51' && <Marvel />}
       <div className='container mx-auto py-8 relative'>
         <div className='back-button'>
           <Link href='/'>
-            <h2 className='py-2 text-3xl font-bold'>
-              Day {children.props.childPropSegment}
-            </h2>
+            <h2 className='py-2 text-3xl font-bold'>Day {day}</h2>
             <span className='bg'></span>
           </Link>
         </div>
