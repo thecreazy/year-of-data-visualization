ff8d4ae9c0172e814379b4f12fc2902164db9ef3
thecreazy <canella_riccardo@icloud.com>
2024-04-01 23:21:04 +0200

    chore(ci): disable webpack cache

--- next.config.js
+++ next.config.js
@@ -7,6 +7,16 @@ const nextConfig = {
     serverSourceMaps: false,
   },
   productionBrowserSourceMaps: false,
+  webpack: (config, { dev }) => {
+    if (config.cache && !dev) {
+      config.cache = Object.freeze({
+        type: 'memory',
+      });
+      config.cache.maxMemoryGenerations = 0;
+    }
+    // Important: return the modified config
+    return config;
+  },
 };
 
 module.exports = nextConfig;
