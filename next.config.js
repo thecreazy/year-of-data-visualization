a598433ff15f1f318dc0de5fdd3cb451e71a75e4
thecreazy <canella_riccardo@icloud.com>
2024-04-01 23:05:55 +0200

    chore(ci): disable source maps

--- next.config.js
+++ next.config.js
@@ -4,7 +4,9 @@ const nextConfig = {
   images: { unoptimized: true },
   experimental: {
     webpackBuildWorker: true,
+    serverSourceMaps: false,
   },
+  productionBrowserSourceMaps: false,
 };
 
 module.exports = nextConfig;
