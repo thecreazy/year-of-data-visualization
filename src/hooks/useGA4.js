6da6fd5f26c6387567a7cbaf150a549750095d3d
thecreazy <canella_riccardo@icloud.com>
2024-04-14 12:40:31 +0200

    feat(analytics): push vitals on datalayer

--- src/hooks/useGA4.js
+++ src/hooks/useGA4.js
@@ -5,12 +5,17 @@ import { onCLS, onFCP, onFID, onINP, onLCP, onTTFB } from 'web-vitals';
 
 export default function useGA4() {
   function sendToGoogleAnalytics({ name, delta, value, id }) {
-    window.gtag('event', name, {
-      value: delta,
-      metric_id: id,
-      metric_value: value,
-      metric_delta: delta,
-    });
+    if (!window.dataLayer) window.dataLayer = [];
+    window.dataLayer.push([
+      'event',
+      name,
+      {
+        value: delta,
+        metric_id: id,
+        metric_value: value,
+        metric_delta: delta,
+      },
+    ]);
   }
   useEffect(() => {
     onCLS(sendToGoogleAnalytics);
